977647a0c044dc123e65e33c60a004e1f35f4420
sibkeisuke <167329652+sibkeisuke@users.noreply.github.com>
2024-08-01 21:51:09 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,16 +10,22 @@
   <p>
     <?php
     $nums = [15, 4, 18, 23, 10];
-    echo '昇順にソートします。<br>';
-    sort($nums);
-    foreach ($nums as $num) {
-      echo $num . '<br>';
-    }
-    echo '降順にソートします。<br>';
-    rsort($nums);
-    foreach ($nums as $num) {
-      echo $num . '<br>';
-    }
+    $order = [true, false];
+
+    function sort_2way($array, $order) {
+      if($order) {
+        echo '昇順にソートします。<br>';
+        sort($array);
+      } else {
+        echo '降順にソートします。<br>';
+        rsort($array);
+      }
+      foreach ($array as $num) {
+        echo $num . '<br>';
+      }
+    }    
+    sort_2way($nums, true);
+    sort_2way($nums, false);
     ?>
   </p>
 </body>
